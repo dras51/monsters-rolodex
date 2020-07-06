93a89f85b30b6f193f0fedc80667f631e86e4938
David Olabode <davvido51@gmail.com>
2020-07-06 12:25:25 +0100

    Updates

---
+++ precache-manifest.5b1fd5cfec8503bd075ecb4ed1a6d7fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8534a3f2c7598ddb4e19deb8b3828165",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7fe19cafb3f7a5553819",
+    "url": "/monsters-rolodex/static/css/main.47040eab.chunk.css"
+  },
+  {
+    "revision": "b7025d703d6faac98ebe",
+    "url": "/monsters-rolodex/static/js/2.a0f84a8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a0f84a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe19cafb3f7a5553819",
+    "url": "/monsters-rolodex/static/js/main.014273d7.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
